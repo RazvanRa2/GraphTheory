ce31fb889e6aa0e1c60cc322c2d6f6def58c85ce
Razvan Radoi <razvanradoi97@gmail.com>
2018-05-28 10:54:55 +0300

    add scc Kosaraju algorithm

--- stronglyConnectedComps.cpp
+++ stronglyConnectedComps.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
+#include <stack>
 using namespace std;
 
 const int kNmax = 100005;
@@ -28,10 +28,50 @@ class Task {
 		}
 	}
 
-	vector<vector<int>> get_scc() {
-		vector<vector<int>> scc;
+    void dfs(int source, vector<bool>& visited, vector<int>& topSort) {
+        visited[source] = true;
 
-        
+        for (vector<int>::iterator i = adj[source].begin(); i != adj[source].end(); i++) {
+            if (!visited[*i]) {
+                dfs(*i, visited, topSort);
+            }
+        }
+
+        topSort.push_back(source);
+    }
+
+    void dfs_t(int source, vector<bool>& visited, vector<int>& current_scc) {
+        visited[source] = false;
+        current_scc.push_back(source);
+
+        for (vector<int>::iterator i = adj[source].begin(); i != adj[source].end(); i++) {
+            if (visited[*i]) {
+                dfs_t(*i, visited, current_scc);
+            }
+        }
+    }
+
+	vector<vector<int> > get_scc() {
+		vector<vector<int> > scc;
+        stack<int>stack;
+        vector<bool> visited(n + 1, false);
+
+        vector<int> topSort;
+        topSort.push_back(-1);
+
+        for (int v = 1; v <= n; v++) {
+            if (!visited[v]) {
+                dfs(v, visited, topSort);
+            }
+        }
+
+        for (int i = n; i >= 1; i--) {
+            if (visited[topSort[i]]) {
+                vector<int> current_scc;
+                dfs_t(topSort[i], visited, current_scc);
+                scc.push_back(current_scc);
+            }
+        }
 
 		return scc;
 	}
